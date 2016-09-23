7937f1e45ff73f94b150683b6ed8d0f7199859df
Ubuntu <ubuntu@ip-172-31-16-163.us-west-2.compute.internal>
2016-09-23 10:01:07 +0000

    mindmaps

---
+++ 工作笔记.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1474611935299" ID="ID_247088393" MODIFIED="1474611935299" TEXT="New Mindmap"/>
+</map>
